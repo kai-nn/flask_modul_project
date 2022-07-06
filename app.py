fca18ad103441f054548509f6a665f0083b30e53
kai-nn <73657963+kai-nn@users.noreply.github.com>
2022-07-06 19:29:37 +0300

    Убрал секретные данные

--- app.py
+++ app.py
@@ -1495,11 +1495,11 @@ def mail_send(addr_to, message):
     # http://codius.ru/articles/Python_%D0%9A%D0%B0%D0%BA_%D0%BE%D1%82%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D1%82%D1%8C_%D0%BF%D0%B8%D1%81%D1%8C%D0%BC%D0%BE_%D0%BD%D0%B0_%D1%8D%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BD%D0%BD%D1%83%D1%8E_%D0%BF%D0%BE%D1%87%D1%82%D1%83
     # addr_to   = "kai.nn@mail.ru"                        # Получатель
 
-    # addr_from = "hydromash.bpzmp@gmail.com"  # Адресат
-    # password = "pikinbpzmp"  # Пароль
+    # addr_from = ""  # Адресат
+    # password = ""  # Пароль
 
-    addr_from = "kai.nn@mail.ru"                    # Адресат
-    password  = "hrenovina"                         # Пароль
+    addr_from = ""                    # Адресат
+    password  = ""                         # Пароль
 
     msg = MIMEMultipart()                           # Создаем сообщение
     msg['From'] = addr_from                         # Адресат
